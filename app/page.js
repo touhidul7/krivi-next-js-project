9095e746d80c22bff4f76bfea571044bff848b9b
touhidul-islam-html <ti360109@gmail.com>
2025-03-17 15:33:22 +0600

    hero section

--- app/page.js
+++ app/page.js
@@ -2,13 +2,14 @@ import Achievement from './components/Achievement/Achievement'
 import DemoVedio from './components/DemoVedio/DemoVedio'
 import BoldSteps from './components/BoldSteps/BoldSteps'
 import Insights from './components/Achievement/Insights/Insights'
+import Hero from './components/Herosection/Hero'
 
 export default function page() {
   
   
   return (
     <div className=''>
-      {/* <CommingSoon/> */}
+      <Hero/>
       <Achievement/>
       <BoldSteps/>
       <DemoVedio/>
